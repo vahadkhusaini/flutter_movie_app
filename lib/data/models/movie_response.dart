9d2aa36aa4ce796ff15f6be6f07f7cc0c79c193e
Vahad Khusaini <vahadkhusaini@gmail.com>
2025-04-15 21:50:47 +0700

    fix bug search

--- lib/data/models/movie_response.dart
+++ lib/data/models/movie_response.dart
@@ -7,9 +7,8 @@ class MovieResponse extends Equatable {
   MovieResponse({required this.movieList});
 
   factory MovieResponse.fromJson(Map<String, dynamic> json) => MovieResponse(
-        movieList: List<MovieModel>.from((json["results"] as List)
-            .map((x) => MovieModel.fromJson(x))
-            .where((element) => element.posterPath != null)),
+        movieList: List<MovieModel>.from(
+            (json["results"] as List).map((x) => MovieModel.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
--- lib/presentation/pages/tv_series_page.dart
+++ lib/presentation/pages/tv_series_page.dart
@@ -13,7 +13,6 @@ import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
 import 'package:ditonton/presentation/state/tv_list_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 class TvSeriesPage extends StatefulWidget {
   static const ROUTE_NAME = '/tv-home';
